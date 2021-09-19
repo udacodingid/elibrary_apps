85fecc17441bd6f65069155027b256362e799f7d
Udacoding Indonesia <78523253+udacodingid@users.noreply.github.com>
2021-09-19 11:42:46 +0700

    update provider

---
+++ elibrary_apps/lib/provider/list_books_provider.dart
@@ -0,0 +1,38 @@
+import 'package:elibrary_apps/utils/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:elibrary_apps/model/model_list_books.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+
+class ListBooksProvider extends ChangeNotifier{
+
+  List<Datum> listData = [];
+
+  bool loading = false;
+
+  ListBooksProvider(context){
+    getListBooks(context);
+  }
+
+  //methode login untuk get data
+
+  Future<void> getListBooks(BuildContext context) async{
+    loading = true;
+    notifyListeners();
+
+    http.Response res = await http.get(Uri.parse(Constant.BASEURL + 'getDataBooks'));
+
+    if(res.statusCode == 200){
+      List<Datum> dataBooksList = modelListBooksFromJson(res.body).data;
+
+      listData = dataBooksList;
+      loading = false;
+      notifyListeners();
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error Get Data")));
+    }
+
+
+  }
+}
---
+++ elibrary_apps/lib/utils/constant.dart
@@ -0,0 +1,6 @@
+
+class Constant{
+
+  static String BASEURL = "http://172.16.0.6/server_elibrary/index.php/ApiMobile/";
+  static String PATH_IMAGE = "http://172.16.0.6/server_elibrary/gambar/";
+}
