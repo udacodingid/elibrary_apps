4c7f0dd6fe2fa7d7b0ffe0e3e4535689d32ce71f
Udacoding Indonesia <78523253+udacodingid@users.noreply.github.com>
2021-09-25 10:43:52 +0700

    update constant

--- elibrary_apps/lib/utils/constant.dart
+++ elibrary_apps/lib/utils/constant.dart
@@ -3,5 +3,5 @@ class Constant{
 
   static String BASEURL = "http://172.16.0.5./server_elibrary/index.php/ApiMobile/";
   static String PATH_IMAGE = "http://172.16.0.5./server_elibrary/gambar/";
-  static String PATH_FILE = "http://172.16.0.5./server_elibrary/gambar/pdf_file";
+  static String PATH_FILE = "http://172.16.0.5./server_elibrary/gambar/pdf_file/";
 }
